34b4c599c0e85f4130c4a74d846f0a6a34d40045
Silver Roi Valdez Ramos <47856708+silverlightning2019@users.noreply.github.com>
2022-04-25 10:56:48 +0800

    Add files via upload

--- Week 1/Introduction to Programming Course/Algorithm/Algorithm V/maxMinAvg.js
+++ Week 1/Introduction to Programming Course/Algorithm/Algorithm V/maxMinAvg.js
@@ -1,33 +1,43 @@
-function MaxMinAvg(arr){
-  var arrnew=[];
-  var max=0;
-  var min=0;
-  var sum=0;
-  var avg=0;
-  for(var i=0; i<arr.length-1; i++){
-    if(arr[i]>arr[i+1]){
-      var temp = arr[i];
-      arr[i] = arr[i+1];
-      arr[i+1] = temp;
+function maxMinAvg(arr){
+  
+  var arrnew = [];
+  var max = 0;
+  var min = 0;
+  var sum = 0;
+  var avg = 0;
+  
+  for(var i = 0; i  < arr.length - 1; i++){
+    
+    for (var j = 0; j < arr.length - i - 1; j++){
+      
+      if (arr[j] > arr[j + 1]){
+        
+        var temp = arr[j];
+        arr[j] = arr[j+1];
+        arr[j+1] = temp;
+      
+      }
+    
     }
+    
   }
+  
   max=arr[i];
   arrnew.push(max);
-  for (var j=0; j<arr.length-1; j++){
-    if (arr[j]<arr[j+1]){
-      var temp1 = arr[j];
-      arr[j] = arr[j+1];
-      arr[j+1] = temp1;
-    }
-  }
-  min=arr[j];
+  
+  min=arr[0];
   arrnew.push(min);
   
-  for(var k=0; k<arr.length; k++){
-    sum+=arr[k];
+  for(var k = 0; k < arr.length; k++){
+    
+    sum += arr[k];
+    
   }
-  avg=sum/arr.length;
+  
+  avg = sum / arr.length;
   arrnew.push(avg);
   return arrnew;
+  
 }
-console.log(MaxMinAvg([0, 2, 4]));
+
+console.log(maxMinAvg([0, 2, 4]));
